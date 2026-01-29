466a32db3e99ef5693e7575adfbf6a15e986f72c
gpizziniSTM <giuseppe.pizzini@stmitalia.com>
2026-01-29 17:56:08 +0100

    Initial Commit

---
+++ Bonjourliquid/BonjourliquidApp.swift
@@ -0,0 +1,17 @@
+//
+//  BonjourliquidApp.swift
+//  Bonjourliquid
+//
+//  Created by Giuseppe Pizzini on 29/01/26.
+//
+
+import SwiftUI
+
+@main
+struct BonjourliquidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
